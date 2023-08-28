5966d6621c9e15f271de8bb8e74b3c1db4f06b56
dmitry <69000015+Degrater@users.noreply.github.com>
2023-08-28 20:25:04 +0300

    new change for mobile version

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@ const counter = document.getElementById('clicker_start');
 const top_Gun_Button = document.getElementById('addToCartButton');
 const romanza_Button = document.getElementById('addToCartButton0');
 const burger = document.querySelector('.burger')
+const burger_0 = document.querySelector('.burger_0')
+const burger_menu = document.getElementById('burger_menu')
 
 let count = localStorage.getItem('count') || 0;
 
@@ -25,11 +27,23 @@ romanza_Button.addEventListener('click' , ()=>{
   }
 });
 
-function checkbox(){
-  if (burger.checked){
-    
-  };
+function onburger(){
+  burger_menu.style.display = 'block';
+  burger.style.display = 'none';
 };
+
+function onburger_0(){
+  burger_menu.style.display = 'none';
+  burger.style.display = 'block';
+};
+
+burger.addEventListener('click' , ()=>{
+  onburger();
+});
+
+burger_0.addEventListener('click',()=>{
+  onburger_0();
+});
 
 
 
